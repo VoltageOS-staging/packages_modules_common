34195119f34dfd7f77a92144f86da3fd15fef848
Pedro Loureiro <pedroql@google.com>
2025-11-14 05:42:00 -0800

    Add support for bugreport zip files
    Bug: 406235465
    
    Flag: EXEMPT HOST_MODE
    
    Test: manual
    Change-Id: I11d8c62d3232b843424f86bf399991e78f014c13

--- tools/mainline_beta/device_config_dump_parser.py
+++ tools/mainline_beta/device_config_dump_parser.py
@@ -16,9 +16,14 @@
 #
 
 import argparse
+import io
 import re
 import sys
 from typing import TextIO
+import zipfile
+
+
+_verbose: bool = False
 
 # Regex to match lines like "namespace/key=value"
 # Group 1: namespace
@@ -69,10 +74,14 @@ def parse_device_config_dumpsys(
       if not found_start:
         if is_start_of_flags(line):
           found_start = True
+          if _verbose:
+            print("Found start of section")
         continue
 
       # check if we got to the end of the device_config dumpsys block
       if is_end_of_dumpsys(line):
+        if _verbose:
+          print("Found end of section")
         return parsed_flags
 
       # We can get all sorts of text from the output. Including values that span multiple lines, etc.
@@ -83,11 +92,68 @@ def parse_device_config_dumpsys(
         parsed_flags.append(
             {"namespace": namespace, "key": key, "value": value}
         )
-  except Exception as e:
-    print(f"An error occurred while reading the file: {e}", file=sys.stderr)
-    sys.exit(1)
 
   return parsed_flags
+
+
+def handle_zip_file(file_path: str) -> list[dict]:
+  """Handles the parsing of a zip file to extract flag values.
+
+  It will try to find the main txt file that contains the bugreport logs and
+  then process it to parse flags.
+  """
+  try:
+    with zipfile.ZipFile(file_path, "r") as z:
+      bugreport_entry = find_bugreport_inside_zip(z)
+      if _verbose:
+        print(f"handle_zip_file: {bugreport_entry=}")
+
+      with z.open(bugreport_entry) as bugreport_file:
+        # ignore errors as there are weird bytes in some files.
+        contents = io.TextIOWrapper(bugreport_file, encoding="utf-8", errors="ignore")
+        return parse_device_config_dumpsys(contents)
+  except zipfile.BadZipFile:
+    print(f"Error: {file_path} is not a valid zip file.", file=sys.stderr)
+    sys.exit(1)
+
+
+def find_bugreport_inside_zip(z: zipfile.ZipFile) -> str:
+  """Tries to find the main txt file inside the zip that contains the logs.
+
+  First it tries to find a file "main_entry.txt", read its contents which
+  should be another filename, and then try to open that file.
+
+  As a fallback, if "main_entry.txt" doesn't exist, it tries to open the
+  first file with a name with the format "bugreport-*.txt".
+  """
+  if "main_entry.txt" in z.namelist():
+    with z.open("main_entry.txt") as main_entry:
+      return main_entry.read().decode("utf-8").strip()
+  else:
+    print(
+        "The zipfile doesn't contain a main_entry.txt file. Attempting"
+        " fallback.",
+        file=sys.stderr,
+    )
+    for name in z.namelist():
+      if name.startswith("bugreport-") and name.endswith(".txt"):
+        return name
+  print(
+      "Could not find bugreport inside the zip file",
+      file=sys.stderr
+  )
+  sys.exit(1)
+
+
+def handle_txt_file(file_path: str) -> list[dict]:
+  file_arg = sys.stdin.fileno() if file_path == "-" else file_path
+  try:
+    # ignore errors as there are weird bytes in some files.
+    with open(file_arg, "r", encoding="utf-8", errors="ignore") as file:
+      return parse_device_config_dumpsys(file)
+  except FileNotFoundError:
+    print(f"Error: File not found at {file_path}", file=sys.stderr)
+    sys.exit(1)
 
 
 def main():
@@ -96,25 +162,38 @@ def main():
   )
   parser.add_argument(
       "file",
-      help="Path to the DeviceConfig dump file. Use '-' for stdin.",
-      type=argparse.FileType("r", encoding="utf-8"),
+      help=(
+          "Path to the file to analyse. Passing a zip file implies it is a "
+          "bug report. Use '-' for stdin. Zip files over stdin are not supported."
+      ),
   )
-
+  parser.add_argument(
+      "--verbose",
+      action="store_true",
+      help="Print more information",
+  )
   args = parser.parse_args()
 
-  with args.file as file:
-    flags = parse_device_config_dumpsys(file)
-    if flags:
-      print("Parsed DeviceConfig Flags:")
-      for flag in flags:
-        print(
-            f"  Namespace: {flag['namespace']}, Key: {flag['key']}, Value:"
-            f" {flag['value']}"
-        )
-    else:
+  global _verbose
+  _verbose = args.verbose
+  is_zip = args.file != "-" and zipfile.is_zipfile(args.file)
+  if is_zip:
+    # handle zip file
+    flags = handle_zip_file(args.file)
+  else:
+    flags = handle_txt_file(args.file)
+
+  if flags:
+    print("Parsed DeviceConfig Flags:")
+    for flag in flags:
       print(
-          "No DeviceConfig flags found in the file matching the expected format."
+          f"  Namespace: {flag['namespace']}, Key: {flag['key']}, Value:"
+          f" {flag['value']}"
       )
+  else:
+    print(
+        "No DeviceConfig flags found in the file matching the expected format."
+    )
 
 
 if __name__ == "__main__":
